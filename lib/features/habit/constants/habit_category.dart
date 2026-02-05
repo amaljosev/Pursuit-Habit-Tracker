db5b2f23fbc5e037c4739e7d04eb45c0e3cc2b48
solutionsinfoway <amaljvattakkunnel@gmail.com>
2026-02-05 16:11:37 +0530

    finaly habit library set completed

--- lib/features/habit/constants/habit_category.dart
+++ lib/features/habit/constants/habit_category.dart
@@ -1,10 +1,10 @@
 class HabitCategory {
   static List<Map<String, dynamic>> categories = [
-    {'id': 0, 'name': 'Health', 'icon': 1938},
-    {'id': 1, 'name': 'Fitness', 'icon': 1},
-    {'id': 2, 'name': 'Growth', 'icon': 3},
-    {'id': 3, 'name': 'Productivity', 'icon': 10},
-    {'id': 4, 'name': 'Reduce', 'icon': 2103},
-    {'id': 5, 'name': 'Quit', 'icon': 2236},
+    {'id': 0, 'name': 'Popular', 'icon': 625},
+    {'id': 1, 'name': 'Health', 'icon': 1243}, 
+    {'id': 2, 'name': 'Fitness', 'icon': 0}, 
+    {'id': 3, 'name': 'Growth', 'icon': 51}, 
+    {'id': 4, 'name': 'Productivity', 'icon': 50}, 
+    {'id': 5, 'name': 'Quit', 'icon': 1387}, 
   ];
 }
